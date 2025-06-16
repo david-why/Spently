857d7553992684ab17d09b964892158a2bda275b
david-why <david_why@outlook.com>
2025-06-16 15:37:33 +0800

    Initial Commit

---
+++ Spently/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spently
+//
+//  Created by David Wang on 2025/6/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spently/SpentlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpentlyApp.swift
+//  Spently
+//
+//  Created by David Wang on 2025/6/16.
+//
+
+import SwiftUI
+
+@main
+struct SpentlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
